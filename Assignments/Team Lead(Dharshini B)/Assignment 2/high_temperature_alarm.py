bff25bb0b038e7e66baf4331a999990d066e27ae
Dharshini-B <84831320+Dharshini-B@users.noreply.github.com>
2022-09-22 22:13:30 +0530

    Update high_temperature_alarm.py

--- Assignments/Team Lead(Dharshini B)/Assignment 2/high_temperature_alarm.py
+++ Assignments/Team Lead(Dharshini B)/Assignment 2/high_temperature_alarm.py
@@ -1,9 +1,9 @@
-/*Assignment 2:
+"""Assignment 2:
 
 Build a python code, 
 Assume u get temperature and humidity values (generated with random function to a variable) and 
 write a condition to continuously detect alarm in case of high temperature.
-*/
+"""
 
 //Solution
 import random
