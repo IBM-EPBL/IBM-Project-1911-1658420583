02cedd6dabb7dfa0d327ff7cc75a73eb8c0506d5
Dharshini-B <84831320+Dharshini-B@users.noreply.github.com>
2022-09-22 22:11:16 +0530

    Update high_temperature_alarm.py

--- Assignments/Team Lead(Dharshini B)/Assignment 2/high_temperature_alarm.py
+++ Assignments/Team Lead(Dharshini B)/Assignment 2/high_temperature_alarm.py
@@ -1,1 +1,17 @@
+/*Assignment 2:
 
+Build a python code, 
+Assume u get temperature and humidity values (generated with random function to a variable) and 
+write a condition to continuously detect alarm in case of high temperature.
+*/
+
+//Solution
+import random
+
+tempValue = random.randint(1,100)
+print("Current temperature  : ",tempValue)
+if(tempValue >= 70):
+    print("Alert ! High temperature sensed .")
+else:
+    print("Normal temperature")
+    
