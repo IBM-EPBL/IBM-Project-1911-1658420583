80b979c2a564d53ab0abc9f0158f18d4d1ba7f8a
JAYAPRIYA28 <83283403+JAYAPRIYA28@users.noreply.github.com>
2022-09-26 23:16:56 +0530

    updated high_temperature_alarm.py

--- Assignments/Team Member 3(Jayapriya G)/Assignment 2/high_temperature_alarm.py
+++ Assignments/Team Member 3(Jayapriya G)/Assignment 2/high_temperature_alarm.py
@@ -6,10 +6,15 @@ Assume u get temperature and humidity values (generated with random function to 
 write a condition to continuously detect alarm in case of high temperature.
 """
 import random
-
-tempValue = random.randint(1,100)
-print("Current temperature  : ",tempValue)
-if(tempValue >= 50):
-    print("Alert ! High temperature sensed .")
-else:
-    print("Normal temperature")
+x = int(input("Enter Threshold Temperature(Range 0,50):"))
+a=1
+while(a==1):
+    value=random.randint(0,50)
+    print("Random Temperature: ",value)
+    if value>x:
+        print("Random Temp:",value,"higher than",x,"(Threshold Temperature)")
+    elif x>value:
+        print("Random Temp:",value,"less than",x,"(Threshold Temperature)")
+    else:
+        print("Random Temp:",value,"equals to",x,"(Threshold Temperature)")
+    a=int(input("Enter 1 to Repeat:"))
