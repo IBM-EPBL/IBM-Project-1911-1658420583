290601f7d4f7bd926fe768f47dcf174dc84a527a
Jayameenakshi <89153251+Jayameenakshi-S@users.noreply.github.com>
2022-09-29 11:21:43 +0530

    Update high temperature alarm

--- Assignments/Team Member 2(Jayameenakshi S)/Assignment 2/high_temperature_alarm.py
+++ Assignments/Team Member 2(Jayameenakshi S)/Assignment 2/high_temperature_alarm.py
@@ -7,12 +7,15 @@ write a condition to continuously detect alarm in case of high temperature.
 """
 
 //Solution
-import random
-
-tempValue = random.randint(1,100)
-print("Current temperature  : ",tempValue)
-if(tempValue >= 70):
-    print("Alert ! High temperature sensed .")
-else:
-    print("Normal temperature")
+import random
+Threshold=int(input("Enter threshold temperature (range 0,100): "))
+choice=1
+while(choice == 1):
+    tempValue = random.randint(1,100)
+    print("Current temperature  : ",tempValue)
+    if(tempValue >= Threshold):
+        print("Alert ! High temperature sensed .",tempValue," is greater than or equal to " ,Threshold)
+    else:
+        print("Normal temperature .",tempValue," is lesser than " ,Threshold)
+    choice=int(input("Enter 1 to continue :"))
     
